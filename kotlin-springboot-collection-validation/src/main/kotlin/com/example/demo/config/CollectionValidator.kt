13f2b533282db46da5209fdbebb839db12045d3a
maple <944123439@qq.com>
2019-07-30 17:29:20 +0800

    springboot集合校验

--- kotlin-springboot-collection-validation/src/main/kotlin/com/example/demo/config/CollectionValidator.kt
+++ kotlin-springboot-collection-validation/src/main/kotlin/com/example/demo/config/CollectionValidator.kt
@@ -12,26 +12,26 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
  * @version 1.0
  * @since 2019-07-30 16:48
  */
-    class CollectionValidator(private val validatorFactory: LocalValidatorFactoryBean) : Validator {
+class CollectionValidator(private val validatorFactory: LocalValidatorFactoryBean) : Validator {
 
-        override fun supports(clazz: Class<*>): Boolean {
-            return Collection::class.java.isAssignableFrom(clazz)
-        }
+    override fun supports(clazz: Class<*>): Boolean {
+        return Collection::class.java.isAssignableFrom(clazz)
+    }
 
-        /**
-         * 校验集合 遇到失败即退出
-         *
-         * @param target 受校验对象
-         * @param errors 错误结果
-         */
-        override fun validate(target: Any, errors: Errors) {
-            val collection = target as Collection<*>
-            for (`object` in collection) {
-                `object`?.let { ValidationUtils.invokeValidator(validatorFactory, `object`, errors) }
-                // 存在错误即退出校验
-                if (errors.hasErrors()) {
-                    break
-                }
+    /**
+     * 校验集合 遇到失败即退出
+     *
+     * @param target 受校验对象
+     * @param errors 错误结果
+     */
+    override fun validate(target: Any, errors: Errors) {
+        val collection = target as Collection<*>
+        for (`object` in collection) {
+            `object`?.let { ValidationUtils.invokeValidator(validatorFactory, `object`, errors) }
+            // 存在错误即退出校验
+            if (errors.hasErrors()) {
+                break
             }
         }
     }
+}
